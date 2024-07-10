b75f265b443c9c074c87e60bc8e7575e28ebf587
xiaoka6666 <115566356+xiaoka6666@users.noreply.github.com>
2024-07-10 22:44:43 +0800

    Create extract-files.sh

---
+++ device/xiaomi/k30u/extract-files.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DEVICE=k30u
+VENDOR=xiaomi
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+ANDROID_ROOT="${MY_DIR}/../../.."
+
+HELPER="${ANDROID_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Write headers and guards
+write_headers
+
+# The standard blobs
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Finish
+write_footers
